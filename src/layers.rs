f5f21a3283eaf8d0fb9b8d233ffe9df02c374016
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-06-21 15:17:04 -0700

    Update layers.rs

--- src/layers.rs
+++ src/layers.rs
@@ -1,6 +1,9 @@
 impl Layer for Tanh{
 	fn backward(&mut self,input:&[Value],inputgrad:&mut [Value],outputgrad:&[Value]){
-		todo!()
+		input.iter().cloned().zip(outputgrad.iter().cloned()).zip(inputgrad.iter_mut()).for_each(|((i,og),ig)|*ig=i.map_2(|i,og|{
+			let o=i.tanh();
+			(1.0-o*o)*og
+		},og))
 	}
 	fn forward(&self,input:&[Value],output:&mut [Value]){input.iter().cloned().zip(output.iter_mut()).for_each(|(i,o)|*o=i.map(f32::tanh))}
 }
