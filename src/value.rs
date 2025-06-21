0db85ad967fd4ef9dbafb8be765928a305485d46
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-06-21 15:16:38 -0700

    Update value.rs

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,5 @@
 /// gets mutable data
-fn buffer_mut<'a>(buffer:&'a mut Arc<[f32]>,count:usize,offset:&mut usize)->&'a mut [f32]{
+fn buffer_mut<'a,E:Clone>(buffer:&'a mut Arc<[E]>,count:usize,offset:&mut usize)->&'a mut [E]{
 	if Arc::strong_count(&*buffer)==1{return &mut Arc::get_mut(buffer).unwrap()[*offset..count+*offset]}
 	let b=Arc::from(&buffer[*offset..count+*offset]);
 	*buffer=b;
@@ -10,7 +10,6 @@ impl Value{
 	/// creates from data and dimensions
 	pub fn from_data<A:AsRef<[f32]>,B:AsRef<[usize]>>(data:A,dims:B)->Self{
 		let (data,dims)=(data.as_ref(),dims.as_ref());
-		let (buffer,dims):(Arc<[f32]>,Arc<[usize]>)=(Arc::from(data),Arc::from(dims));
 		let mut strides=vec![0;dims.len()];
 		let offset=0;
 
@@ -26,8 +25,21 @@ impl Value{
 		buffer_mut(&mut self.buffer,self.count,&mut self.offset).iter_mut().for_each(|x|*x=f(*x));
 		self
 	}
+	/// applies the function to each pair of components
+	pub fn map_2<F:Fn(f32,f32)->f32>(mut self,f:F,r:Value)->Value{
+		todo!()
+	}
+	/// applies broadcasting
+	pub fn try_broadcast(mut self,dims:&[usize])->Option<Value>{
+		if dims.iter().zip(self.dims.iter()).any(|(b,d)|b!=d&&d!=&1){return None}
+		let (dl,sl)=(self.dims.len(),self.strides.len());
+		let (d,s)=(buffer_mut(&mut self.dims,dl,&mut 0),buffer_mut(&mut self.strides,sl,&mut 0));
 
+		d.iter_mut().zip(dims.iter()).zip(s.iter_mut()).for_each(|((d,b),s)|if d!=b{(*d,*s)=(*b,0)});
+		Some(self)
+	}
 }
+
 #[derive(Clone,Debug,Default)]
 /// nn layer io value. stores data and dimensions
 pub struct Value{buffer:Arc<[f32]>,count:usize,dims:Arc<[usize]>,offset:usize,strides:Arc<[usize]>}
